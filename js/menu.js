ae585c0777ced1d6b11d1fd22589a49e5a05b0b6
Sergio Alvarez <144191639+serskynet@users.noreply.github.com>
2024-07-05 12:25:51 -0500

    Add files via upload

---
+++ js/menu.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Crear el menú desplegable
+    var menuDesplegable = document.createElement("select");
+    menuDesplegable.id = "opciones";
+  
+    // Crear opciones del menú
+    var opciones = [
+      { text: "Menu", value: "" },
+      { text: "ir a reproductor de audio", value: "./reproductorDeAudio.html" },
+      { text: "ir a reproducro de video", value: "./reproductor de video.html" },
+      { text: "ir a pagina web.", value: "./index.html" },
+      
+    ];
+  
+    opciones.forEach(function(opcion) {
+      var opcionElement = document.createElement("option");
+      opcionElement.textContent = opcion.text;
+      opcionElement.value = opcion.value;
+      menuDesplegable.appendChild(opcionElement);
+    });
+  
+    // Añadir evento para redireccionar al usuario cuando cambia la opción seleccionada
+    menuDesplegable.addEventListener("change", function() {
+      var selectedOption = this.value;
+      if (selectedOption !== "") {
+        window.location.href = selectedOption;
+      }
+    });
+  
+    // Agregar el menú desplegable al documento
+    document.getElementById("menu_desplegable").appendChild(menuDesplegable);
+  });
+  
