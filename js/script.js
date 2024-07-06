8075725cf4359b37f1bb96f069e5da3efaf5804c
Sergio Alvarez <144191639+serskynet@users.noreply.github.com>
2024-07-06 17:32:05 -0500

    Delete js directory

--- js/script.js
+++
@@ -1,8 +0,0 @@
-
-document.addEventListener('DOMContentLoaded', function() {
-    var player = videojs('myVideo');
-    player.src({
-      src: 'http://stream.flynetwifi.com:1935/live/mobile-062/playlist.m3u8', // Aquí debes colocar la URL de tu stream HLS
-      type: 'application/x-mpegURL'
-    });
-  });
