a5b1844ed2d1b44855d1e56e58cb5dc1767e8721
Sergio Alvarez <144191639+serskynet@users.noreply.github.com>
2024-07-06 17:33:59 -0500

    Add files via upload

---
+++ js/cambiarCanales.js
@@ -0,0 +1,121 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Referencia al campo de entrada y al botón en el HTML
+    //var urlInput = document.getElementById('urlInput');
+    
+    var cambiarCanal1 = document.getElementById('cambiarCanal1');
+    var cambiarCanal2 = document.getElementById('cambiarCanal2');
+    var cambiarCanal3 = document.getElementById('cambiarCanal3');
+    var cambiarCanal4 = document.getElementById('cambiarCanal4');
+    var cambiarCanal5 = document.getElementById('cambiarCanal5');
+    var cambiarCanal6 = document.getElementById('cambiarCanal6');
+    var cambiarCanal7 = document.getElementById('cambiarCanal7');
+    var cambiarCanal8 = document.getElementById('cambiarCanal8');
+    var cambiarCanal9 = document.getElementById('cambiarCanal9');
+    var cambiarCanal10 = document.getElementById('cambiarCanal10');
+
+
+    var canal1  =   28;
+    var canal2  =   139;
+    var canal3  =   9;
+    var canal4  =   82;
+    var canal5  =   126;
+    var canal6  =   132;
+    var canal7  =   134;
+    var canal8  =   143;
+    var canal9  =   19;
+    var canal10  =   7;
+
+    cambiarCanal1.addEventListener('click', function() {
+
+        var cambiar = canal1;
+
+        console.log("canal".cambiar);
+        reproductor(cambiar);
+
+    });
+
+    cambiarCanal2.addEventListener('click', function() {
+
+        var cambiar = canal2;
+
+        console.log("canal".cambiar);
+        reproductor(cambiar);
+
+    });
+
+    cambiarCanal3.addEventListener('click', function() {
+
+        var cambiar = canal3;
+
+        console.log("canal".cambiar);
+        reproductor(cambiar);
+
+    });
+
+    cambiarCanal4.addEventListener('click', function() {
+
+        var cambiar = canal4;
+
+        console.log("canal".cambiar);
+        reproductor(cambiar);
+
+    });
+
+    
+    cambiarCanal5.addEventListener('click', function() {
+
+        var cambiar = canal5;
+
+        console.log("canal".cambiar);
+        reproductor(cambiar);
+
+    });
+
+    cambiarCanal6.addEventListener('click', function() {
+
+        var cambiar = canal6;
+
+        console.log("canal".cambiar);
+        reproductor(cambiar);
+
+    });
+
+    cambiarCanal7.addEventListener('click', function() {
+
+        var cambiar = canal7;
+
+        console.log("canal".cambiar);
+        reproductor(cambiar);
+
+    });
+
+    cambiarCanal8.addEventListener('click', function() {
+
+        var cambiar = canal8;
+
+        console.log("canal".cambiar);
+        reproductor(cambiar);
+
+    });
+
+    cambiarCanal9.addEventListener('click', function() {
+
+        var cambiar = canal9;
+
+        console.log("canal".cambiar);
+        reproductor(cambiar);
+
+    });
+
+    cambiarCanal10.addEventListener('click', function() {
+
+        var cambiar = canal10;
+
+        console.log("canal".cambiar);
+        reproductor(cambiar);
+
+    });
+    
+
+   
+});
